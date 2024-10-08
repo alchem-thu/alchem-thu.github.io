cba70f527127f674635ebfcb7621e88907071e05
ygm <ygm@zhihu.com>
2024-10-08 16:11:25 +0800

    initial commit

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#scp *.html chen3385@antor.cs.purdue.edu:/p/p24/sites/alchem/sdf3423rfsfasfawfafaf234sfafvdsvsfawerf3w534adfascg7980979hihknkji723sdfsfdsf
+#scp -r static chen3385@antor.cs.purdue.edu:/p/p24/sites/alchem/sdf3423rfsfasfawfafaf234sfafvdsvsfawerf3w534adfascg7980979hihknkji723sdfsfdsf 
+
+scp *.html chen3385@antor.cs.purdue.edu:/p/p24/sites/alchem
+scp -r static chen3385@antor.cs.purdue.edu:/p/p24/sites/alchem
